9f4d04f01cfe5cfa2543d49b581250f14ba756fc
Ricardo Erick Rebêlo <jacknpoe@yahoo.com>
2023-12-26 17:48:54 -0300

    Add files via upload

--- jacknpoe.sql
+++ jacknpoe.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: sql206.infinityfree.com
--- Tempo de geração: 19/12/2023 às 10:29
+-- Tempo de geração: 26/12/2023 às 15:16
 -- Versão do servidor: 10.4.17-MariaDB
 -- Versão do PHP: 7.2.22
 
@@ -40,24 +40,24 @@ CREATE TABLE `menu` (
 --
 
 INSERT INTO `menu` (`CD_MENU`, `NM_DESCRICAO`, `NM_LINK`, `NM_IMAGEM`) VALUES
-(1, 'Teste de Parser que separa endere&ccedil;o em logradouro, n&uacute;mero e complemento', 'php_testeparser.php', 'php_testeparser.png'),
-(2, 'Teste de Calculadora entre juros e acr&eacute;scimo', 'php_testejuros.php', 'php_testejuros.png'),
+(1, 'Teste de Parser que separa endereço em logradouro, número e complemento', 'php_testeparser.php', 'php_testeparser.png'),
+(2, 'Teste de Calculadora entre juros e acréscimo', 'php_testejuros.php', 'php_testejuros.png'),
 (3, 'Teste de Roladores de dados para RPGs de mesa', 'php_testedices.php', 'php_testedices.png'),
-(4, 'C&aacute;lculo da diferen&ccedil;a entre duas datas', 'php_calculadatas.php', 'php_calculadatas.png'),
-(5, 'C&aacute;lculo da f&oacute;rmula de Bhaskara', 'php_calculabhaskara.php', 'php_calculabhaskara.png'),
-(6, 'Teste de Valida&ccedil;&atilde;o de CPF', 'php_testecpf.php', 'php_testecpf.png'),
-(7, 'Calculadora de IMC (&Iacute;ndice de Massa Corp&oacute;rea)', 'php_testeimc.php', 'php_testeimc.png'),
-(8, 'Sequ&ecirc;ncia de Fibonacci', 'php_fibonacci.php', 'php_fibonacci.png'),
-(9, 'Teste de conversor de ASCII para bin&aacute;rio e de bin&aacute;rio para ASCII', 'php_testeconversorbinario.php', 'php_testeconversorbinario.png'),
-(10, 'Teste da classe academia: Consultar exerc&iacute;cio', 'php_testeacademia.php', 'php_testeacademia.png'),
+(4, 'Cálculo da diferença entre duas datas', 'php_calculadatas.php', 'php_calculadatas.png'),
+(5, 'Cálculo da fórmula de Bhaskara', 'php_calculabhaskara.php', 'php_calculabhaskara.png'),
+(6, 'Teste de Validação de CPF', 'php_testecpf.php', 'php_testecpf.png'),
+(7, 'Calculadora de IMC (Índice de Massa Corpórea)', 'php_testeimc.php', 'php_testeimc.png'),
+(8, 'Sequência de Fibonacci', 'php_fibonacci.php', 'php_fibonacci.png'),
+(9, 'Teste de conversor de ASCII para binário e de binário para ASCII', 'php_testeconversorbinario.php', 'php_testeconversorbinario.png'),
+(10, 'Teste da classe academia: Consultar exercício', 'php_testeacademia.php', 'php_testeacademia.png'),
 (11, 'Teste da classe academia 2: Incluir aluno', 'php_testeacademia2.php', 'php_testeacademia2.png'),
 (12, 'Teste de Gerador de Senhas', 'php_testegerasenha.php', 'php_testegerasenha.png'),
 (13, 'Retira aspas simples contra code injection (MariaDB e mySQL)', 'php_retiraaspas.php', 'php_retiraaspas.png'),
-(14, 'C&aacute;lculo da f&oacute;rmula de Bhaskara (em JavaScript)', 'js_calculabhaskara.html', 'js_calculabhaskara.png'),
-(15, 'Analisador de N&uacute;meros (em JavaScript)', 'js_analisavalores.html', 'js_analisavalores.png'),
-(16, 'Exerc&iacute;cio de Calculadora (em JavaScript)', 'aula70.html', 'aula70.png'),
-(17, 'Exerc&iacute;cio de Bolas (em JavaScript)', 'aula82.html', 'aula82.png'),
-(18, 'Teste de Calculadora entre juros e acr&eacute;scimo (em JavaScript)', 'js_testejuros.html', 'js_testejuros.png');
+(14, 'Cálculo da fórmula de Bhaskara (em JavaScript)', 'js_calculabhaskara.html', 'js_calculabhaskara.png'),
+(15, 'Analisador de Números (em JavaScript)', 'js_analisavalores.html', 'js_analisavalores.png'),
+(16, 'Exercício de Calculadora (em JavaScript)', 'aula70.html', 'aula70.png'),
+(17, 'Exercício de Bolas (em JavaScript)', 'aula82.html', 'aula82.png'),
+(18, 'Teste de Calculadora entre juros e acréscimo (em JavaScript)', 'js_testejuros.html', 'js_testejuros.png');
 
 --
 -- Índices de tabelas apagadas
--- menu.php
+++ menu.php
@@ -10,24 +10,26 @@
 	namespace jacknpoe;
 
 	// cabeçalhos para tratar os caracteres e definir a aplicação como JSON
-	require_once( 'configuracoes.php');
-	$cabecalho = "Content-Type: text/html; charset=" . CARACTERES;
-	header( $cabecalho, true);
+	header("Content-Type: text/html; charset=UTF-8", true);
 	header('Content-Type: application/json');
 
 	// Tenta conectar no banco, se retornar erro, morre
-	require_once( 'connect.php');
-	$conexao = new \mysqli( $hostname, $username, $password, $jacknpoe);
-	if ($conexao->connect_errno) { die( json_encode( "Falha ao conectar: (" . $conexao->connect_errno . ") " . $conexao->connect_error)); }
+	require_once('connect.php');
+	$conexao = new \mysqli($hostname, $username, $password, $jacknpoe);
+	if ($conexao->connect_errno) { die(json_encode("Falha ao conectar: (" . $conexao->connect_errno . ") " . $conexao->connect_error)); }
+
+	// coloca os resultados para serem UTF-8
+	$consulta = $conexao->query("SET character_set_results = utf8");
+	if ($conexao->errno) { die(json_encode("Falha ao setar: (" . $conexao->errno . ") " . $conexao->error)); }
 
 	// faz a consulta dos itens de menu do banco, se der um erro, morre
-	$consulta = $conexao->query( "SELECT NM_DESCRICAO, NM_LINK, NM_IMAGEM FROM menu ORDER BY CD_MENU");
-	if ($conexao->errno) { die( json_encode( "Falha ao consultar: (" . $conexao->errno . ") " . $conexao->error));	}
+	$consulta = $conexao->query("SELECT NM_DESCRICAO, NM_LINK, NM_IMAGEM FROM menu ORDER BY CD_MENU");
+	if ($conexao->errno) { die(json_encode("Falha ao consultar: (" . $conexao->errno . ") " . $conexao->error)); }
 	$conexao->close();
 
 	// inclui os itens dinâmicos do menu em $colunas
 	$colunas = $consulta->fetch_all(MYSQLI_ASSOC);
  	// inclui o Teste de PHP in-line, que sempre estará fixo no final
-	array_push( $colunas, [ "NM_DESCRICAO" => "Teste de PHP in-line", "NM_LINK" => "php_teste.php", "NM_IMAGEM" => "php_teste.png"]);
-	echo json_encode( $colunas, JSON_UNESCAPED_UNICODE);
+	array_push($colunas, ["NM_DESCRICAO" => "Teste de PHP in-line", "NM_LINK" => "php_teste.php", "NM_IMAGEM" => "php_teste.png"]);
+	echo json_encode($colunas, JSON_UNESCAPED_UNICODE);
 ?>
