9d1d76606241917313bb7bef4f2772c34d2540c7
Ricardo Erick Rebêlo <jacknpoe@yahoo.com>
2024-01-03 18:05:23 -0300

    Add files via upload

--- jacknpoe.sql
+++ jacknpoe.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: sql206.infinityfree.com
--- Tempo de geração: 03/01/2024 às 14:43
+-- Tempo de geração: 03/01/2024 às 15:56
 -- Versão do servidor: 10.4.17-MariaDB
 -- Versão do PHP: 7.2.22
 
@@ -40,24 +40,24 @@ CREATE TABLE `menu` (
 --
 
 INSERT INTO `menu` (`CD_MENU`, `NM_DESCRICAO`, `NM_LINK`, `NM_IMAGEM`) VALUES
-(1, 'Teste de Parser que separa endereço em logradouro, número e complemento • PHP', 'php_testeparser.php', 'php_testeparser.png'),
-(2, 'Teste de Calculadora entre juros e acréscimo • PHP', 'php_testejuros.php', 'php_testejuros.png'),
-(3, 'Teste de Roladores de dados para RPGs de mesa • PHP', 'php_testedices.php', 'php_testedices.png'),
+(1, 'Parser que separa endereço em logradouro, número e complemento • PHP', 'php_testeparser.php', 'php_testeparser.png'),
+(2, 'Calculadora entre juros e acréscimo • PHP', 'php_testejuros.php', 'php_testejuros.png'),
+(3, 'Roladores de dados para RPGs de mesa • PHP', 'php_testedices.php', 'php_testedices.png'),
 (4, 'Cálculo da diferença entre duas datas • PHP', 'php_calculadatas.php', 'php_calculadatas.png'),
 (5, 'Cálculo da fórmula de Bhaskara • PHP', 'php_calculabhaskara.php', 'php_calculabhaskara.png'),
-(6, 'Teste de Validação de CPF • PHP', 'php_testecpf.php', 'php_testecpf.png'),
-(7, 'Teste de Calculadora de IMC (Índice de Massa Corpórea) • PHP', 'php_testeimc.php', 'php_testeimc.png'),
+(6, 'Validação de CPF • PHP', 'php_testecpf.php', 'php_testecpf.png'),
+(7, 'Calculadora de IMC (Índice de Massa Corpórea) • PHP', 'php_testeimc.php', 'php_testeimc.png'),
 (8, 'Sequência de Fibonacci • PHP', 'php_fibonacci.php', 'php_fibonacci.png'),
-(9, 'Teste de conversor de ASCII para binário e de binário para ASCII • PHP', 'php_testeconversorbinario.php', 'php_testeconversorbinario.png'),
-(10, 'Teste da classe academia: Consultar exercício • PHP', 'php_testeacademia.php', 'php_testeacademia.png'),
-(11, 'Teste da classe academia 2: Incluir aluno • PHP', 'php_testeacademia2.php', 'php_testeacademia2.png'),
-(12, 'Teste de Gerador de Senhas • PHP', 'php_testegerasenha.php', 'php_testegerasenha.png'),
+(9, 'Conversor de ASCII para binário e de binário para ASCII • PHP', 'php_testeconversorbinario.php', 'php_testeconversorbinario.png'),
+(10, 'Classe academia: Consultar exercício • PHP', 'php_testeacademia.php', 'php_testeacademia.png'),
+(11, 'Classe academia 2: Incluir aluno • PHP', 'php_testeacademia2.php', 'php_testeacademia2.png'),
+(12, 'Gerador de senhas • PHP', 'php_testegerasenha.php', 'php_testegerasenha.png'),
 (13, 'Retira aspas simples contra code injection (MariaDB e mySQL) • PHP', 'php_retiraaspas.php', 'php_retiraaspas.png'),
 (14, 'Cálculo da fórmula de Bhaskara • JavaScript', 'js_calculabhaskara.html', 'js_calculabhaskara.png'),
 (15, 'Analisador de Números • JavaScript', 'js_analisavalores.html', 'js_analisavalores.png'),
 (16, 'Exercício de Calculadora • JavaScript', 'aula70.html', 'aula70.png'),
 (17, 'Exercício de Bolas • JavaScript', 'aula82.html', 'aula82.png'),
-(18, 'Teste de Calculadora entre juros e acréscimo • JavaScript', 'js_testejuros.html', 'js_testejuros.png'),
+(18, 'Calculadora entre juros e acréscimo • JavaScript', 'js_testejuros.html', 'js_testejuros.png'),
 (19, 'Cálculo da fórmula de Bhaskara • API em PHP + JavaScript', 'api_calculabhaskara.html', 'api_calculabhaskara.png');
 
 --
